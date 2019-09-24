f6f20dfdab1f23f6256c7798072a60f000bd3f83
Yasodha Sundaresan <yasodhasundaresan@Yasodhas-MBP.attlocal.net>
2019-09-24 16:15:58 -0400

    Commit script file

---
+++ script.js
@@ -0,0 +1,29 @@
+var a;
+var student;
+var sum;
+var str;
+var arr;
+var z;
+
+
+//Datatypes
+//primitives
+var a="yash";
+a=123;
+a=null;
+a=true;
+
+console.log(Object.prototype.toString.call(a));
+//non -primitives
+var student=[1,"yash",true];
+a2={name:'yash',ssn:12345};
+
+console.log(Object.prototype.toString.call(student));
+console.log(typeof a2);
+
+function addNumbers(x,y){
+    return x+y;
+}
+
+var sum=addNumbers(10,20);
+console.log("TCL: sum", sum)
